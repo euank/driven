8130f4f68897d243cdf4042fe36ff63c597e2fc2
Euan Kemp <euan@ngrok.com>
2019-09-24 22:07:38 +0900

    parser: implement simple drivenfile parser

--- driven_parser/src/lib.rs
+++ driven_parser/src/lib.rs
@@ -1,17 +1,18 @@
-#[macro_use]
 extern crate nom;
 
 use nom::{
-    bytes::complete::{escaped_transform, is_not, take},
-    character::complete::{char, one_of},
-    combinator::{cut, map, map_res, map_parser},
-    error::{context,  ParseError, ErrorKind},
+    branch::alt,
+    bytes::complete::{escaped_transform, is_not, tag, take, take_while1},
+    character::complete::{char, multispace0, one_of, space0},
+    combinator::{cut, map, map_parser, map_res},
+    error::{context, ErrorKind, ParseError},
+    multi,
     sequence::{preceded, terminated},
     IResult,
 };
 
-// assumes the '\' has been parsed already, parses the character proceeding it.
-fn parse_str_escape_char<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
+// parses the escape character(s) after a '\' in quotes
+fn str_escape<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
     let take1 = take(1usize);
     let unescape_char = |c: char| -> Result<&str, E> {
         match c {
@@ -19,27 +20,91 @@ fn parse_str_escape_char<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a 
             '"' => Ok("\""),
             '$' => Ok("$"),
             '\\' => Ok("\\"),
-            _ => Err(ParseError::add_context(i, "expected a valid backslash escape character", ParseError::from_error_kind(i, ErrorKind::OneOf))),
+            _ => Err(ParseError::add_context(
+                i,
+                "expected a valid backslash escape character",
+                ParseError::from_error_kind(i, ErrorKind::OneOf),
+            )),
         }
     };
     let unescape = map_res(one_of("n\\\""), unescape_char);
     map_parser(take1, unescape)(i)
 }
 
-fn parse_str_part<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, StringPart, E> {
+fn str_part<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, StringPart, E> {
     // TODO: variables
     // "${foo}" must interact well with escaping, such as "\${foo}", too
-    let string_lit = escaped_transform(is_not("\\\""), '\\', parse_str_escape_char);
+    let string_lit = escaped_transform(is_not("\\\""), '\\', str_escape);
     map(string_lit, |s: String| StringPart::Literal(s))(i)
+}
+
+fn is_variable_char(i: &str) -> IResult<&str, &str> {
+    take_while1(move |c: char| c.is_alphanumeric() || c == '_')(i)
+}
+
+fn unquoted_variable_name(i: &str) -> IResult<&str, StringRef> {
+    let varname = is_variable_char;
+    map(varname, |s: &str| StringRef {
+        parts: vec![StringPart::Literal(s.to_string())],
+    })(i)
 }
 
 fn quoted_string(i: &str) -> IResult<&str, StringRef> {
     let refparser = context(
         "string",
-        preceded(char('\"'), cut(terminated(parse_str_part, char('\"')))),
+        preceded(char('\"'), cut(terminated(str_part, char('\"')))),
     );
-    // TODO: parse multiple parts
+    // TODO: parse multiple parts, only relevant when we support variable references
     map(refparser, |s: StringPart| StringRef { parts: vec![s] })(i)
+}
+
+fn variable_name(i: &str) -> IResult<&str, StringRef> {
+    alt((unquoted_variable_name, quoted_string))(i)
+}
+
+fn wrapped<I, O1, O2, E: ParseError<I>, F, G>(first: F, wrapper: G) -> impl Fn(I) -> IResult<I, O1, E>
+where
+    F: Fn(I) -> IResult<I, O1, E>,
+    G: Fn(I) -> IResult<I, O2, E>,
+    G: Copy,
+{
+    terminated(preceded(wrapper, first), wrapper)
+}
+
+fn variable_assignment(i: &str) -> IResult<&str, DrivenVar> {
+    let (i, _) = multispace0(i)?;
+    let whitespace_var_name = wrapped(variable_name, space0);
+    let internal = wrapped(tag("internal"), space0);
+    let internal_var = map(terminated(preceded(internal, whitespace_var_name), tag("=")), |res| (true, res));
+
+    let whitespace_var_name = wrapped(variable_name, space0);
+    let regular_var = map(terminated(whitespace_var_name, tag("=")), |res| (false, res));
+
+    let (i, (internal, var)) = alt((internal_var, regular_var))(i)?;
+    let (i, val) = terminated(preceded(space0, quoted_string), multispace0)(i)?;
+
+    Ok((
+        i,
+        DrivenVar {
+            internal: internal,
+            name: var,
+            value: val,
+        },
+    ))
+}
+
+fn driven_file(i: &str) -> IResult<&str, DrivenFile> {
+    // TODO: ignore_parents and allow_shell_exec
+    let (i, vars) = multi::many0(variable_assignment)(i)?;
+
+    Ok((
+        i,
+        DrivenFile {
+            ignore_parents: false,
+            allow_shell_exec: false,
+            variables: vars,
+        },
+    ))
 }
 
 #[derive(Debug, PartialEq)]
@@ -75,33 +140,87 @@ mod test {
     use super::*;
     use nom::Err;
 
+    fn lit(s: &str) -> StringRef {
+        StringRef {
+            parts: vec![StringPart::Literal(s.to_string())],
+        }
+    }
+
     #[test]
-    fn parse_str_part_test() {
+    fn str_part_test() {
         assert_eq!(
-            parse_str_part::<()>("foo"),
+            str_part::<()>("foo"),
             Ok(("", StringPart::Literal("foo".to_string())))
         );
         assert_eq!(
-            parse_str_part::<()>(r#"foo\"a"#),
+            str_part::<()>(r#"foo\"a"#),
             Ok(("", StringPart::Literal(r#"foo"a"#.to_string())))
         );
         assert_eq!(
-            parse_str_part::<()>(r#"foo\\foo"#),
+            str_part::<()>(r#"foo\\foo"#),
             Ok(("", StringPart::Literal(r#"foo\foo"#.to_string())))
         );
         assert_eq!(
-            parse_str_part::<(&str, ErrorKind)>(r#"foo\1foo"#),
+            str_part::<()>("ご飯"),
+            Ok(("", StringPart::Literal("ご飯".to_string())))
+        );
+        assert_eq!(
+            str_part::<(&str, ErrorKind)>(r#"foo\1foo"#),
             Err(Err::Error(("1", ErrorKind::OneOf))),
         );
     }
 
     #[test]
     fn quoted_str_test() {
+        assert_eq!(quoted_string(r#""foo""#), Ok(("", lit("foo"))),)
+    }
+
+    #[test]
+    fn variable_assignment_test() {
         assert_eq!(
-            quoted_string(r#""foo""#),
-            Ok(("", StringRef{
-                parts: vec![StringPart::Literal("foo".to_string())],
-            }))
-        )
+            variable_assignment(r#"foo="bar""#),
+            Ok((
+                "",
+                DrivenVar {
+                    internal: false,
+                    name: lit("foo"),
+                    value: lit("bar"),
+                }
+            ))
+        );
+        assert_eq!(
+            variable_assignment(r#""foo"="bar""#),
+            Ok((
+                "",
+                DrivenVar {
+                    internal: false,
+                    name: lit("foo"),
+                    value: lit("bar"),
+                }
+            ))
+        );
+        assert_eq!(
+            variable_assignment(r#" "foo" = "bar" "#),
+            Ok((
+                "",
+                DrivenVar {
+                    internal: false,
+                    name: lit("foo"),
+                    value: lit("bar"),
+                }
+            ))
+        );
+        assert_eq!(
+            variable_assignment(r#" internal "foo" = "bar" "#),
+            Ok((
+                "",
+                DrivenVar {
+                    internal: true,
+                    name: lit("foo"),
+                    value: lit("bar"),
+                }
+            ))
+        );
+        assert!(variable_assignment(" foo = \n \"bar\" ").is_err(),);
     }
 }
